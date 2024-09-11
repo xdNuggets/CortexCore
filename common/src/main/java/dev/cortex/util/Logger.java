2ff2d5fd2e0d86901cf4fd8ca31b9d68db2e0a03
xdNuggets <joshpatg2005@gmail.com>
2024-09-11 16:38:54 +0100

    Commit Numero Uno

---
+++ common/src/main/java/dev/cortex/util/Logger.java
@@ -0,0 +1,16 @@
+package dev.cortex.util;
+
+public class Logger {
+
+
+    private String prefix;
+
+    public Logger() {
+        prefix = "[CortexCore]";
+    }
+
+    public void log(String message) {
+
+    }
+
+}
---
+++ src/main/java/dev/cortex/cortexcore/CortexCore.java
@@ -0,0 +1,19 @@
+package dev.cortex.cortexcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CortexCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
